6e9f468fbfd63a85faf1bbc6479256b774a7be98
Andrea Cattaneo <acattaneo@nexapp.it>
2019-05-06 14:15:51 +0200

    Test spam filtering

--- src/js/site.js
+++ src/js/site.js
@@ -8,7 +8,21 @@ $('[download]').on('click', function() {
 $('.modal form').submit(function(e) {
   e.preventDefault();
   var $form = $(this);
-  $.post('https://forms.hubspot.com/uploads/form/v2/', $form.serialize()).then();
+  $.post(
+    'https://forms.hubspot.com/uploads/form/v2/',
+    $form.serialize()
+  ).then();
   $('.modal .modal-body:nth-child(1)').hide();
   $('.modal .modal-body:nth-child(2)').show();
+});
+
+$('#contact-form').submit(function(e) {
+  e.preventDefault();
+  var $form = $(this);
+  $.post(
+    window.location,
+    $form.serialize()
+  ).then();
+  $form.addClass('hidden');
+  $('success-contact-form').removeClass('hidden');
 });
