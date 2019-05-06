5074a5ab16c95b870053becebe510d9b6574ceb3
Andrea Cattaneo <acattaneo@nexapp.it>
2019-05-06 12:54:28 +0200

    commit

--- src/js/site.js
+++ src/js/site.js
@@ -5,17 +5,10 @@ $('[download]').on('click', function() {
   $('.modal').modal('hide');
 });
 
-function showModalSuccess() {
-  $('.modal .modal-body:nth-child(1)').hide();
-  $('.modal .modal-body:nth-child(2)').show();
-}
-
 $('.modal form').submit(function(e) {
   e.preventDefault();
-
   var $form = $(this);
-
-  $.post('https://forms.hubspot.com/uploads/form/v2/', $form.serialize())
-    .then(showModalSuccess)
-    .catch(showModalSuccess);
+  $.post('https://forms.hubspot.com/uploads/form/v2/', $form.serialize()).then();
+  $('.modal .modal-body:nth-child(1)').hide();
+  $('.modal .modal-body:nth-child(2)').show();
 });
