43e0116c27dc1106f60ad14f9ca025ca3e39f62a
abbo <thomas.abbondi@promo.it>
2021-09-07 10:16:43 +0200

    Aggiunto invio dati form verso script PHP esterno

--- src/js/site.js
+++ src/js/site.js
@@ -5,13 +5,13 @@ $('[download]').on('click', function () {
   $('.modal').modal('hide');
 });
 
+var url = 'https://www.ortea.it/next-email.php';
+
 $('.modal form').submit(function (e) {
   e.preventDefault();
   var $form = $(this);
-  $.post(
-    window.location,
-    $form.serialize()
-  ).then();
+  $.post(window.location, $form.serialize()).then();
+  $.post(url, $form.serialize()).then();
   $('.modal .modal-body:nth-child(1)').hide();
   $('.modal .modal-body:nth-child(2)').show();
 });
@@ -19,26 +19,17 @@ $('.modal form').submit(function (e) {
 $('#contact-form').submit(function (e) {
   e.preventDefault();
   var $form = $(this);
-  $.post(
-    window.location,
-    $form.serialize()
-  ).then();
+  $.post(window.location, $form.serialize()).then();
+  $.post(url, $form.serialize()).then();
   $form.addClass('d-none');
   $('#success-contact-form').removeClass('d-none');
 });
 
-(function () {
-  var form = document.querySelector('form#mc-embedded-subscribe-form');
-  if (typeof form !== 'undefined') {
-    var email = form.querySelector('input[type="email"]');
-    form.addEventListener('submit', function (event) {
-      event.preventDefault();
-
-      if (email.value.trim() !== '') {
-        this.submit();
-      } else {
-        email.focus();
-      }
-    });
-  }
-}());
+$('#newsletter-form').submit(function (e) {
+  e.preventDefault();
+  var $form = $(this);
+  $.post(window.location, $form.serialize()).then();
+  $.post(url, $form.serialize()).then();
+  $form.addClass('d-none');
+  $('#success-newsletter-form').removeClass('d-none');
+});
